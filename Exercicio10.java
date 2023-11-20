65996bb173544f2ce295167ee9d05642a389fe4d
Everton Davi marques dos Santos <143417773+Maverick36021@users.noreply.github.com>
2023-11-20 13:38:49 -0300

    Codigo em java utilizando o comando Scanner

---
+++ Exercicio10.java
@@ -0,0 +1,23 @@
+/*O custo de um carro novo ao consumidor é a soma do custo de fábrica com a porcentagem do
+distribuidor e dos impostos (aplicados ao custo de fábrica). Supondo que o percentual do distribuidor
+seja de 28% e os impostos de 45%, escrever um algoritmo para ler o custo de fábrica de um carro,
+calcular e escrever o custo final ao consumidor*/
+
+package ExercicioFaccat;
+
+import java.util.Scanner;
+
+public class Exercicio10 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		double porcentagemDistribuidor = 28, porcentagemGoverno = 45;
+		System.out.println("Digite o valor do custo de fabrica do automovel: ");
+		double custoFabrica = sc.nextDouble();
+		double valorDistribuidor = custoFabrica/100*porcentagemDistribuidor;
+		double valorGoverno = custoFabrica/100*porcentagemGoverno;
+		double custoFinal = custoFabrica+valorDistribuidor+valorGoverno;
+		System.out.printf("O valor final do veiculo fica em R$ %.2f",custoFinal);
+		sc.close();
+	}
+
+}
